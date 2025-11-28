6580f09e549b581443fe94a0e41900602e7e6aa1
nereame96 <nereame96@gmail.com>
2025-11-28 17:12:39 +0100

    feat : implement panel component

--- src/app/components/panel/panel.ts
+++ src/app/components/panel/panel.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, input, signal, WritableSignal } from '@angular/core';
+import { budgetProduct } from '../../../models/budget-product';
+import { FormGroup, ReactiveFormsModule } from "@angular/forms";
+
 
 @Component({
   selector: 'app-panel',
@@ -6,6 +9,21 @@ import { Component } from '@angular/core';
   templateUrl: './panel.html',
   styleUrl: './panel.scss',
 })
-export class Panel {
+export class PanelComponent {
+
+  counterValuePages = signal(0)
+  counterValueLanguages = signal(0)
+
+  increment(valueToUpdate : WritableSignal<number>) {
+    valueToUpdate.update(value => value + 1)
+  }
+  decrement(valueToUpdate : WritableSignal<number>) {
+if (valueToUpdate() > 0) {
+  valueToUpdate.update(value => value - 1)
+
+}
+  }
+
+
 
 }
--- src/app/components/product-item/product-item.ts
+++ src/app/components/product-item/product-item.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { budgetProduct } from '../../../models/budget-product';
 import { FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { PanelComponent } from "./../panel/panel";
 
 @Component({
   selector: 'app-product-item',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, PanelComponent],
   templateUrl: './product-item.html',
   styleUrl: './product-item.scss',
 })
@@ -12,5 +13,10 @@ export class ProductItemComponent {
 
   product = input.required<budgetProduct>()
   parentForm = input.required<FormGroup>()
+
+  isSelected() : boolean {
+    return this.parentForm().get(this.product().id)?.value || false
+
+  }
 
 }
